938f8eb555c8c7fe4902510461d00be0a76dca8d
martin <guerreiromartin@gmail.com>
2024-11-25 19:12:25 -0300

    cambios

---
+++ MiApiRestFul.api/Data/ApiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using MiApiRestFul.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiApiRestFul.api.Data;
+
+public class ApiDbContext : DbContext
+{
+    public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options) {}
+
+    public DbSet<Usuario> Usuarios { get; set; }
+}
